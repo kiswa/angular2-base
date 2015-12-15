3993ff5d5fd08a62573c1bd3e856ad8aea56fb23
kiswa <kiswa.com@gmail.com>
2015-12-15 11:02:48 -0500

    Update to latest Angular2.

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,6 @@ var gulp = require('gulp'),
     src = 'src/',
     dist = 'dist/',
     paths = {
-        tsconfig: src + 'app/tsconfig.json',
         ts: src + 'app/**/*.ts',
         html: src + '**/*.html',
         images: src + 'images/**/*.*',
@@ -60,7 +59,7 @@ gulp.task('styles', function() {
 });
 
 gulp.task('tsc', function() {
-    var tsProject = tsc.createProject(paths.tsconfig),
+    var tsProject = tsc.createProject('tsconfig.json'),
         tsResult = tsProject.src()
             .pipe(tsc(tsProject));
 
