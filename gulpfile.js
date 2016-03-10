2f59c3fa1194a2904c8ed31a0646bd1e50d509c0
kiswa <kiswa.com@gmail.com>
2016-03-10 17:58:52 +0000

    Update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,6 @@ var gulp = require('gulp'),
             js: [
                 'node_modules/angular2/bundles/angular2-polyfills.js',
                 'node_modules/es6-shim/es6-shim.js',
-                'node_modules/systemjs/dist/system-polyfills.src.js',
                 'node_modules/systemjs/dist/system.src.js',
                 'node_modules/rxjs/bundles/Rx.js',
                 'node_modules/angular2/bundles/angular2.dev.js'
