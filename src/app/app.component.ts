bc150f67b3b06e7f0e8e106aa910b3ca9fa48663
kiswa <kiswa.com@gmail.com>
2016-09-01 20:05:22 +0000

    Update to Angular 2 RC6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ROUTER_DIRECTIVES } from '@angular/router';
 
 import { ExampleComponent } from './example/example.component';
 
@@ -16,8 +15,7 @@ import { ExampleComponent } from './example/example.component';
             <a [routerLink]="['/about']">About</a>
         </p>
         <router-outlet></router-outlet>
-    </div>`,
-    directives: [ ROUTER_DIRECTIVES ]
+    </div>`
 })
 export class AppComponent {
     public isTestable: boolean = true;
